2e0dac2ecb8f4d36d7b921416b8ffccaf0715900
Thales Marzarotto <Thalesmarzarotto@gmail.com>
2022-11-25 12:43:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {  useEffect } from 'react';
 import styles from '../styles/Home.module.css'
-import Link  from 'next/Link'
+import Link  from 'next/link'
 import {AiFillInstagram} from 'react-icons/ai'
 import {AiFillFacebook} from 'react-icons/ai'
 import {FaTiktok} from 'react-icons/fa'
